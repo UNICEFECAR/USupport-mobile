adc2e7f6c50a92fea6ade7c06c840ca40a40527a
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-01-11 23:38:56 +0200

    Add: exports in index.js

--- src/components/inputs/index.js
+++ src/components/inputs/index.js
@@ -3,3 +3,5 @@ export * from "./RadioButton";
 export * from "./Input";
 export * from "./InputPassword";
 export * from "./InputSearch";
+export * from "./CheckBoxSelector";
+export * from "./RadioButtonSelector";
