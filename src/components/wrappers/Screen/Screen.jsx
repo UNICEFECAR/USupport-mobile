181845136160d1a6d50268ff0ce6f67706b75cfe
Georgi 7DIGIT <georgi@7digit.io>
2023-01-13 15:01:20 +0200

    Add: spiral-background to the Screen component

--- src/components/wrappers/Screen/Screen.jsx
+++ src/components/wrappers/Screen/Screen.jsx
@@ -5,24 +5,32 @@ import {
   Platform,
   View,
   StatusBar,
+  Image,
 } from "react-native";
 
 import { ButtonOnlyIcon } from "../../buttons";
 import { appStyles } from "#styles";
+import { RadialGradient } from "react-native-gradients";
+import spiralBackground from "../../../assets/spiral-background.png";
 
 // Main wrapper for every screen
-function Screen({
+export function Screen({
   children,
   style,
   isBackgroundColorEnabled = true,
   outsideComponent,
+  hasRadialGradient = true,
   hasEmergencyButton = true,
+  hasSpiralBackground = true,
 }) {
+  const colorList = [
+    { offset: "0%", color: "#ebe0ff", opacity: "0.55" },
+    { offset: "100%", color: "#ebe0ff", opacity: "0" },
+  ];
   return (
     <SafeAreaView
       style={[
         styles.screen,
-        style,
         isBackgroundColorEnabled ? styles.screenBackground : "",
       ]}
     >
@@ -31,7 +39,17 @@ function Screen({
         backgroundColor={"transparent"}
         translucent={Platform.OS === "android" ? true : false}
       />
+
       <View style={[styles.screenChildren, style]}>{children}</View>
+
+      {hasSpiralBackground && (
+        <Image
+          source={spiralBackground}
+          style={styles.spiralImage}
+          resizeMode="stretch"
+        />
+      )}
+
       {hasEmergencyButton && (
         <ButtonOnlyIcon
           style={styles.emergencyButton}
@@ -47,18 +65,26 @@ const styles = StyleSheet.create({
   screen: {
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     flex: 1,
-    zIndex: 1,
     position: "relative",
   },
-  screenBackground: { backgroundColor: appStyles.colorGray100 },
+  screenBackground: { backgroundColor: appStyles.colorWhite_ff },
   screenChildren: {
     flex: 1,
+    // paddingHorizontal: 16,
+  },
+  radialGradient: {
+    position: "absolute",
+    top: 0,
+    left: -50,
+    width: "50%",
+    height: "50%",
+    zIndex: -1,
   },
   emergencyButton: {
     position: "absolute",
     bottom: 16,
     right: 16,
+    zIndex: 999,
   },
+  spiralImage: { width: "100%", position: "absolute", bottom: 0, zIndex: 999 },
 });
-
-export default Screen;
