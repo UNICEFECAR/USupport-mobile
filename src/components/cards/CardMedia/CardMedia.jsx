71b5aefde4a8ba6055ea7d4a37d3b5c3ee0cd886
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-01-18 19:23:19 +0200

    Create: CardMedia component

---
+++ src/components/cards/CardMedia/CardMedia.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+
+import { AppText } from "../../texts/AppText/AppText";
+import { Icon } from "../../icons/Icon";
+import { AppButton } from "../../buttons/AppButton/AppButton";
+
+import { appStyles } from "#styles";
+
+/**
+ * CardMedia
+ *
+ * CardMedia component to be used to display article details
+ *
+ * @return {jsx}
+ */
+export const CardMedia = ({
+  image,
+  title,
+  creator,
+  readingTime,
+  onPress,
+  style,
+}) => {
+  return (
+    <View style={[appStyles.shadow2, styles.cardMedia, style]}>
+      <Image
+        source={image ? image : "https://picsum.photos/343/400"}
+        style={styles.image}
+      />
+      <View style={styles.textContainer}>
+        <AppText namedStyle="h3">{title}</AppText>
+        <View style={styles.creatorContainer}>
+          <AppText namedStyle="smallText">By {creator}</AppText>
+          <Icon
+            size="sm"
+            name="time"
+            color={appStyles.colorGray_66768d}
+            style={styles.icon}
+          />
+          <AppText namedStyle="smallText">{readingTime} min read</AppText>
+        </View>
+        <AppButton
+          type="secondary"
+          label="Read more"
+          size="sm"
+          style={styles.readMoreButton}
+          onPress={() => {
+            onPress && onPress();
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardMedia: {
+    width: "96%",
+    maxWidth: 420,
+    borderRadius: 24,
+    backgroundColor: appStyles.colorWhite_ff,
+    ...appStyles.shadow2,
+    borderWidth: 1,
+    borderColor: "transparent",
+  },
+  image: {
+    width: "100%",
+    height: 160,
+  },
+  textContainer: {
+    padding: 16,
+  },
+  creatorContainer: { flexDirection: "row", marginTop: 8 },
+  icon: { marginLeft: 16, marginRight: 5 },
+  readMoreButton: { marginTop: 16 },
+});
