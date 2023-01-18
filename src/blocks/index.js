1230cd25e64786380dfd86534618434a65e0dedf
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-01-18 02:36:26 +0200

    Create: Login Screen and block

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -4,3 +4,6 @@ export * from "./PrivacyPolicy";
 export * from "./Welcome";
 export * from "./RegisterEmail";
 export * from "./RegisterAnonymous";
+// export * from "./UserProfile";
+// export * from "./UserDetails";
+export * from "./Login";
--- src/blocks/locales.js
+++ src/blocks/locales.js
@@ -4,3 +4,6 @@ export * as PrivacyPolicy from "./PrivacyPolicy/locales";
 export * as RegisterEmail from "./RegisterEmail/locales";
 export * as RegisterAnonymous from "./RegisterAnonymous/locales";
 export * as Welcome from "./Welcome/locales.js";
+// export * as UserProfile from "./UserProfile/locales";
+// export * as UserDetails from "./UserDetails/locales";
+export * as Login from "./Login/locales";
