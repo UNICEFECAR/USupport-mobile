78a58215f6ecc5a72117b7b362d0a80d46cf1870
Georgi 7DIGIT <georgi@7digit.io>
2023-05-16 00:28:24 +0300

    Fix: dont show SOSCenter button in the PrivacyPolicy if the user is not logged in

--- src/screens/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/screens/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Screen } from "#components";
 import { PrivacyPolicy as PrivacyPolicyBlock } from "#blocks";
+import { Context } from "#services";
 
 /**
  * PrivacyPolicy
@@ -10,8 +11,9 @@ import { PrivacyPolicy as PrivacyPolicyBlock } from "#blocks";
  * @returns {JSX.Element}
  */
 export const PrivacyPolicy = ({ navigation }) => {
+  const { token } = useContext(Context);
   return (
-    <Screen>
+    <Screen hasEmergencyButton={!!token}>
       <PrivacyPolicyBlock navigation={navigation} />
     </Screen>
   );
