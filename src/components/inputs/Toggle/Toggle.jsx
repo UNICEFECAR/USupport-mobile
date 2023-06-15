4158e5db407a0f65594abb17f015bae8bdd9aa2e
Georgi 7DIGIT <georgi@7digit.io>
2023-06-15 15:44:47 +0300

    Add: label to the Toggle component

--- src/components/inputs/Toggle/Toggle.jsx
+++ src/components/inputs/Toggle/Toggle.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Switch } from "react-native";
-
+import { Switch, View } from "react-native";
+import { AppText } from "../../texts/AppText";
 import { appStyles } from "#styles";
 
 /**
@@ -10,21 +10,39 @@ import { appStyles } from "#styles";
  *
  * @return {jsx}
  */
-export const Toggle = ({ isToggled, handleToggle, style, ...props }) => {
+export const Toggle = ({
+  isToggled,
+  handleToggle,
+  label,
+  labelStyle,
+  style,
+  wrapperStyles,
+  ...props
+}) => {
   return (
-    <Switch
-      trackColor={{
-        false: appStyles.colorGray_ea,
-        true: appStyles.colorSecondary_9749fa,
-      }}
-      thumbColor={appStyles.colorWhite_ff}
-      ios_backgroundColor={
-        isToggled ? appStyles.colorSecondary_9749fa : appStyles.colorGray_ea
-      }
-      onValueChange={handleToggle}
-      value={isToggled}
-      style={style}
-      {...props}
-    />
+    <View style={wrapperStyles}>
+      {label && (
+        <AppText
+          style={[{ color: appStyles.colorBlue_3d527b }, labelStyle]}
+          namedStyle="text"
+        >
+          {label}
+        </AppText>
+      )}
+      <Switch
+        trackColor={{
+          false: appStyles.colorGray_ea,
+          true: appStyles.colorSecondary_9749fa,
+        }}
+        thumbColor={appStyles.colorWhite_ff}
+        ios_backgroundColor={
+          isToggled ? appStyles.colorSecondary_9749fa : appStyles.colorGray_ea
+        }
+        onValueChange={handleToggle}
+        value={isToggled}
+        style={style}
+        {...props}
+      />
+    </View>
   );
 };
