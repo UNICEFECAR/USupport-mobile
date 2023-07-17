5778105d0e33476d2c2d47431c725629d5649d47
Georgi 7DIGIT <georgi@7digit.io>
2023-07-17 15:30:43 +0300

    Fix: wrong sex value in UserDetails

--- src/blocks/UserDetails/UserDetails.jsx
+++ src/blocks/UserDetails/UserDetails.jsx
@@ -129,7 +129,7 @@ export const UserDetails = ({
     { label: t("sex_male"), value: "male" },
     { label: t("sex_female"), value: "female" },
     { label: t("sex_unspecified"), value: "unspecified" },
-    { label: t("sex_none"), value: "none" },
+    { label: t("sex_none"), value: "notMentioned" },
   ];
 
   const urbanRuralOptions = [
