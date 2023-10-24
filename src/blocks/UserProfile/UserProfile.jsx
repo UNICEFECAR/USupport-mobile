d3cf3f969a2bf63680513e6350ddbae2ed70b03e
Chavdar Shishkov <chavdar.shishkov123@gmail.com>
2023-10-24 11:35:33 +0300

    Refactor: Fix loading issue

--- src/blocks/UserProfile/UserProfile.jsx
+++ src/blocks/UserProfile/UserProfile.jsx
@@ -33,20 +33,12 @@ export const UserProfile = ({ navigation }) => {
 
   const { isTmpUser, handleRegistrationModalOpen } = useContext(Context);
 
-  const [displayName, setDisplayName] = useState("");
+  const clientQuery = useGetClientData(isTmpUser ? false : true)[0];
+  const clientData = isTmpUser ? {} : clientQuery?.data;
 
-  const clientQueryArray = useGetClientData(isTmpUser ? false : true);
-  const clientData = isTmpUser ? {} : clientQueryArray[0].data;
-
-  useEffect(() => {
-    if (clientData) {
-      if (clientData.name && clientData.surname) {
-        setDisplayName(`${clientData.name} ${clientData.surname}`);
-      } else {
-        setDisplayName(clientData.nickname);
-      }
-    }
-  }, [clientData]);
+  const displayName = clientData?.name
+    ? `${clientData?.name} ${clientData?.surname}`
+    : clientData?.nickname;
 
   const protectedPages = [
     "UserDetails",
@@ -61,8 +53,6 @@ export const UserProfile = ({ navigation }) => {
       navigation.push(redirectTo);
     }
   };
-
-  const handleLogout = useLogout();
 
   return (
     <React.Fragment>
