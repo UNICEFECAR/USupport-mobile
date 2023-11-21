53ce591601cb9b2be62347c8a80951b5ab35a90a
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-11-21 09:21:42 +0200

    Refactor: ConsultationBig provider name number of lines

--- src/components/cards/ConsultationBig/ConsultationBig.jsx
+++ src/components/cards/ConsultationBig/ConsultationBig.jsx
@@ -31,7 +31,8 @@ export const ConsultationBig = ({
   handleAcceptSuggestion,
   t,
 }) => {
-  const { providerName, timestamp, image, status, price } = consultation;
+  const { timestamp, image, status, price } = consultation;
+  let providerName = "Sebastian LongerTigran Galustiyan";
   const imageUrl = AMAZON_S3_BUCKET + "/" + (image || "default");
 
   const isLive = checkIsFiveMinutesBefore(timestamp);
@@ -68,7 +69,9 @@ export const ConsultationBig = ({
               size="sm"
               style={styles.avatar}
             />
-            <AppText style={styles.providerNameText}>{providerName}</AppText>
+            <AppText style={styles.providerNameText} numberOfLines={3}>
+              {providerName}
+            </AppText>
           </View>
           {status === "suggested" ? (
             <AppButton
