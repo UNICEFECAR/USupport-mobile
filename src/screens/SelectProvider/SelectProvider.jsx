3e4c299371c2dc4c8cdad2db0b3f31551bbd08fa
7Digit-Main <it-support@7digit.io>
2025-04-25 12:31:26 +0300

    Fix: dont set isFiltering to true if there are no changes in the filters when pressing "apply" button

--- src/screens/SelectProvider/SelectProvider.jsx
+++ src/screens/SelectProvider/SelectProvider.jsx
@@ -125,7 +125,9 @@ export const SelectProvider = ({ navigation }) => {
   // it back to false when the providers data has been fetched
   const [isFiltering, setIsFiltering] = useState(false);
   const handleFilterSave = (data) => {
-    setIsFiltering(true);
+    if (JSON.stringify(data) !== JSON.stringify(allFilters)) {
+      setIsFiltering(true);
+    }
 
     setAllFilters((prev) => ({ ...prev, ...data }));
 
