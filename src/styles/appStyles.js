3c775e8bc21b590bf8c4f69fee16326d308587a9
7Digit-Main <it-support@7digit.io>
2025-05-10 15:58:53 +0300

    Add: color

--- src/styles/appStyles.js
+++ src/styles/appStyles.js
@@ -55,6 +55,7 @@ export default {
   colorBlue_263238: "#263238",
   colorBlue_20809E_0_3: "rgba(32, 128, 158, 0.3)",
   color_blue_c1d7e0: "#c1d7e0",
+  colorBlue_246FE5: "#246FE5",
 
   colorRed_eb5757: "#eb5757",
   colorRed_cc4c4c: "#cc4c4c",
