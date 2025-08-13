67128863bf1464e0a53c51e08a4fafd83ef10737
Stefan Dobrev <stefandobrev88@gmail.com>
2025-08-13 13:09:36 +0300

    Add: Armenian translation

--- i18n.js
+++ i18n.js
@@ -1,6 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import * as am from "./src/locales/am.json";
 import * as en from "./src/locales/en.json";
 import * as kk from "./src/locales/kk.json";
 import * as pl from "./src/locales/pl.json";
@@ -8,6 +9,7 @@ import * as ru from "./src/locales/ru.json";
 import * as uk from "./src/locales/uk.json";
 
 const resources = {
+  am,
   en,
   kk,
   pl,
