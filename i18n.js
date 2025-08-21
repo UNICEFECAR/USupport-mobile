3dcc4fff1651b7b715e254a199978c6a1e6abe71
7Digit-Main <it-support@7digit.io>
2025-08-21 14:16:53 +0300

    Fix: armenia language code

--- i18n.js
+++ i18n.js
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import * as am from "./src/locales/am.json";
+import * as hy from "./src/locales/hy.json";
 import * as en from "./src/locales/en.json";
 import * as kk from "./src/locales/kk.json";
 import * as pl from "./src/locales/pl.json";
@@ -9,7 +9,7 @@ import * as ru from "./src/locales/ru.json";
 import * as uk from "./src/locales/uk.json";
 
 const resources = {
-  am,
+  hy,
   en,
   kk,
   pl,
