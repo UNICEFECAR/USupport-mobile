2802389c1e5a14c157e8536e1bb35c3f37c4d271
Stefan Dobrev <stefandobrev88@gmail.com>
2025-09-25 17:22:16 +0300

    Romanian translations added

--- i18n.js
+++ i18n.js
@@ -5,14 +5,17 @@ import * as hy from "./src/locales/hy.json";
 import * as en from "./src/locales/en.json";
 import * as kk from "./src/locales/kk.json";
 import * as pl from "./src/locales/pl.json";
+import * as ro from "./src/locales/ro.json";
 import * as ru from "./src/locales/ru.json";
 import * as uk from "./src/locales/uk.json";
+
 
 const resources = {
   hy,
   en,
   kk,
   pl,
+  ro,
   ru,
   uk,
 };
