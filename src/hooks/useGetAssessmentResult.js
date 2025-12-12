7fdeb88dcbc5564c9bdaad54f5082352fc3737ab
7Digit-Main <it-support@7digit.io>
2025-12-12 17:30:11 +0200

    Fix: long loading time in baselineAssesmentResult

--- src/hooks/useGetAssessmentResult.js
+++ src/hooks/useGetAssessmentResult.js
@@ -73,8 +73,7 @@ export function useGetAssessmentResult(queryObj, enabled = true) {
         typeof queryObj.psychological !== "undefined" &&
         typeof queryObj.social !== "undefined" &&
         typeof queryObj.biological !== "undefined",
-      staleTime: 5 * 60 * 1000, // 5 minutes
-      cacheTime: 10 * 60 * 1000, // 10 minutes
+      retry: false,
     }
   );
 
