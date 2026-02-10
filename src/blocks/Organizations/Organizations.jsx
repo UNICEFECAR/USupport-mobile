7715ef27ff9c82049809290f41686f7cfaae1a5f
7Digit-Main <it-support@7digit.io>
2026-02-10 12:37:55 +0200

    Add: userInteraction filter to useGetallOrganizations

--- src/blocks/Organizations/Organizations.jsx
+++ src/blocks/Organizations/Organizations.jsx
@@ -90,6 +90,7 @@ export const Organizations = ({
     search: filters.search,
     district: filters.district,
     paymentMethod: filters.paymentMethod,
+    userInteraction: filters.userInteraction,
     specialisations: filters.specialisations,
   });
 
