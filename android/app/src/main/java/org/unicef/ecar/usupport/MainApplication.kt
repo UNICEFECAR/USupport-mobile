abe96aaa04d8110202135d54abcd10049e049894
7Digit-Main <it-support@7digit.io>
2026-03-05 15:51:02 +0200

    Upgrade expo to sdk 52

--- android/app/src/main/java/org/unicef/ecar/usupport/MainApplication.kt
+++ android/app/src/main/java/org/unicef/ecar/usupport/MainApplication.kt
@@ -1,4 +1,4 @@
-package org.unicef.ecar.usupport;
+package org.unicef.ecar.usupport
 
 import android.app.Application
 import android.content.res.Configuration
@@ -10,22 +10,22 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.ReactHost
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
-
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
-import cl.json.ShareApplication
 
-class MainApplication : Application(), ReactApplication, ShareApplication {
+class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
         this,
         object : DefaultReactNativeHost(this) {
           override fun getPackages(): List<ReactPackage> {
+            val packages = PackageList(this).packages
             // Packages that cannot be autolinked yet can be added manually here, for example:
             // packages.add(new MyReactNativePackage());
-            return PackageList(this).packages
+            return packages
           }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
@@ -42,7 +42,7 @@ class MainApplication : Application(), ReactApplication, ShareApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, false)
+    SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
@@ -53,9 +53,5 @@ class MainApplication : Application(), ReactApplication, ShareApplication {
   override fun onConfigurationChanged(newConfig: Configuration) {
     super.onConfigurationChanged(newConfig)
     ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
-  }
-
-  override fun getFileProviderAuthority(): String {
-    return BuildConfig.APPLICATION_ID + ".provider"
   }
 }
